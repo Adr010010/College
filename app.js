edbb0b86c9a10bedf287fa9840adb4f0b73c3ee5
Adr <aidarkanatbek08@gmail.com>
2025-01-28 19:08:23 +0500

    52братуха

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-let age = 45;
-let name = 'John';
+// function adr () {
+//     console.log('Hello World');
+// }
+// adr();
+// console.log('----------------');
+
+
+// adr1();
+// function adr1 (){
+//     const a = 5;
+//     const b = 100;
+//     const c = a * b;
+//     console.log(c);
+// }
+// const adr4 = function () {
+//     console.log('Hello World');
+// }
+// adr4();
+function adr5 () {
+    let a = 7;
+    if (a === 5) {
+        console.log('Строго ровно 5');
+    } else if (a < 5) {
+        console.log('Меньше 5');
+    }else if (a > 5) {
+        console.log('Больше 5');
+    }  else {
+        console.log('Error');
+    }
+}
+adr5();
