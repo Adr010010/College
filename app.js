889e17a2501f638e314323462c1f97fa7e820e46
Adr <aidarkanatbek08@gmail.com>
2025-01-30 18:43:37 +0500

    Ничего нового

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
-// function adr () {
-//     console.log('Hello World');
+// function adr(name = 'Aidar', surName = 'Kanatbek') {
+//     console.log(alert(`${name} - ${surName}`));
 // }
 // adr();
-// console.log('----------------');
 
+// let i = 0;
+// for (i; i < 11; i++) {
+//     console.log(i);
+// }
+// let adr = 0;
+// while (adr < 10){
+//     console.log(adr);
+//     adr++;
+// }
+// let a = 0;
+// while (a < 20) {
+//     a++;
+//     if (a === 10) continue;
+//     console.log(a);
+// }
+// let i = 1;
+// do {
+//     console.log("В процессе..")
+// } while (i < 10) {
+//     console.log("I меньше 10");
+// }
+// const h1 = document.querySelector("h1");
 
-// adr1();
-// function adr1 (){
-//     const a = 5;
-//     const b = 100;
-//     const c = a * b;
-//     console.log(c);
+// function fn1 (){
+//     h1.textContent = 'Sigma';
+// };
+// function fn2 (){
+//     h1.textContent = 'Batyr';
+// };
+
+// h1.addEventListener("mouseover", fn1);
+// h1.addEventListener("mouseout", fn2);
+// const adr = function (a1 = "h1"){
+//     document.querySelector(a1).textContent = "Nigga";
 // }
-// const adr4 = function () {
-//     console.log('Hello World');
+// adr();
+// function adr (){
+//     console.log(arguments.length);
 // }
-// adr4();
-function adr5 () {
-    let a = 7;
-    if (a === 5) {
-        console.log('Строго ровно 5');
-    } else if (a < 5) {
-        console.log('Меньше 5');
-    }else if (a > 5) {
-        console.log('Больше 5');
-    }  else {
-        console.log('Error');
+// adr('Hello');
+function adr(a) {
+    if (typeof a === 'number'){
+        return 'Namber'
+    } else if (typeof a === 'string') {
+        return 'String'
+    }else {
+        return 'Error'
     }
 }
-adr5();
+console.log(adr(false));
